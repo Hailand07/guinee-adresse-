ea9e647f9b75af6eedf50f15f24548178e5557b9
Hailand07 <mohamedbachirhaidara5@gmail.com>
2026-04-08 20:20:04 +0000

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ const { createClient } = supabase;
 window.supabase = createClient(supabaseUrl, supabaseKey);
 
 async function chargerBoutique() {
-    console.log("Connexion à Supabase...");
-    let { data: products, error } = await window.supabase
-        .from('products')
-        .select('*');
+    console.log("Test d'affichage...");
+    const testDiv = document.getElementById('grid-container');
+    testDiv.innerHTML = "<h1 style='color:gold'>CONNEXION RÉUSSIE !</h1>";
+}
+
 
     if (error) {
         console.error("Erreur de chargement:", error);
